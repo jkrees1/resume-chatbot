97fbe98f6fa78703a37a461d4bab16f9d730b6c8
jk rees <jkrees@syr.edu>
2025-06-18 15:37:49 -0400

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -19,7 +19,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+      <body className="bg-gray-50 text-gray-900 antialiased">
         {children}
       </body>
     </html>
